8c88829c8dc5c14d28586dcf7ad3bfb98a12a11a
root <root@vultr.guest>
2018-04-01 10:35:03 +0000

    use gevent

--- manager.py
+++ manager.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from app import app
+from gevent.wsgi import WSGIServer
+http_server = WSGIServer(('', 5000), app.app)
+http_server.serve_forever()
 
-app.app.run()
